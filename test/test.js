c51436f5403c9c6f8a7c9db4be054a39d75331b0
RicardoTaipe <rdavid28@hotmail.es>
2019-08-09 19:47:15 -0500

    testing node app with mocha and supertest

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const request = require('supertest');
 const app = require('../index.js');
 
-describe('GET / home page', ()=>{
-  it('respond with string \'hello world\'',(done)=>{
+describe('GET / home page', function(){
+  it('respond with string \'hello world\'',function(done){
     request(app).get('/').expect('hello world', done);
   });
 });
